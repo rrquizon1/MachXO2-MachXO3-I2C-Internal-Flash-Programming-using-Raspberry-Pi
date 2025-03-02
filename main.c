7958973a93b867673ea6430e10463af50d01782e
rrquizon1 <70574862+rrquizon1@users.noreply.github.com>
2025-03-02 15:11:37 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <linux/i2c-dev.h>
+#include <i2c/smbus.h>  // Using libi2c-dev for SMBus operations
+#include <string.h>
+#include "rbpi-i2c.h"
+#include "data.h"
+#include <gpiod.h>
+
+int main() {
+    
+    
+
+
+
+    i2c_init();
+ 
+    usleep(1000);
+    device_id(); //i2c read device ID Check
+    isc_enable_sram(); //isc_enable: Enters Programming Mode:SRAM
+    isc_erase_sram(); //SRAM Erase   
+    sr_read();//Status Register Read
+    isc_enable_flash();//ISC_ENABLE: Enters Programming Mode: Flash
+    isc_erase_flash();//ISC_ERASE: Flash
+    sr_read();   //Status Register Read 
+    lsc_init_address();// Address of internal flash set
+    flash_program();//program flash
+    lsc_init_address(); //address of itnernal flash set
+    verify_flash(); // Verify contents of the internal flash
+    program_done(); //program done bit of CFG sector
+    usleep(1000);
+    sr_read();// Read Status Register
+    isc_disable();//ISC_DISABLE: Exits programming mode
+
+
+
+    // Close the I2C bus
+    close(fd);
+    return EXIT_SUCCESS;
+}
